f05a6bedbce1f03f7ca63602dfeed783a82aeb4f
Damien <12201763@student.pxl.be>
2024-04-16 15:50:01 +0200

    init

---
+++ PeopleApp.Api/Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace PeopleApp.Api.Attributes
+{
+
+    [AttributeUsage(validOn: AttributeTargets.Class)]
+    public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string APIKEYNAME = "ApiKey";
+        private ContentResult GetContentResult(int statusCode, string content)
+        {
+            var result = new ContentResult();
+            result.StatusCode = statusCode;
+            result.Content = content;
+            return result;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            try
+            {
+                if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+                {
+                    context.Result = GetContentResult(401, "Api Key was not provided");
+                    return;
+                }
+
+                var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+                if (appSettings == null)
+                {
+                    context.Result = GetContentResult(401, "Appsettings not found");
+                    return;
+                }
+
+                var ApiKey = appSettings.GetValue<string>(APIKEYNAME);
+                if (ApiKey == null)
+                {
+                    context.Result = GetContentResult(401, "Appsettings - ApiKey - not found");
+                    return;
+                }
+
+                if (!ApiKey.Equals(extractedApiKey))
+                {
+                    context.Result = GetContentResult(401, "ApiKey is not valid");
+                    return;
+                }
+                await next();
+            }
+            catch (Exception ex)
+            {
+                context.Result = GetContentResult(401, ex.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ PeopleApp.Mvc/Controllers/DepartmentController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleApp.ClassLib.Models;
+using PeopleApp.Mvc.Services.Interfaces;
+
+namespace PeopleApp.Mvc.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        IDepartmentRepository _repo;
+
+        public DepartmentController(IDepartmentRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var result = await _repo.GetAsync();
+            if (result.Succeeded)
+            {
+                return View(result.Entities);
+            }
+            return View(Enumerable.Empty<Department>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _repo.AddAsync(department);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                ModelState.AddModelError("", result.Error);
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DetailsAsync(long id)
+        {
+            var result = await _repo.GetByIdAsync(id);
+
+            if (result.Succeeded)
+            {
+                return View(result.Entity);
+            }
+
+            ModelState.AddModelError("", result.Error);
+
+            return View();
+        }
+    }
+}
